f9d1c1be4c628b1bed13d8a41a4df7a6e0cb87e7
unknown <SanSid@.(none)>
2011-05-03 09:21:55 -0700

    Add console test add to local repo

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Globalization;
+
+namespace Microsoft.IT.RelationshipManagement.Infrastructure.Instrumentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CultureInfo.CurrentCulture.Name);
+        }
+    }
+}
